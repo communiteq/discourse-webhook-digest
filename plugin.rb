439f8377a4e8fecb2bd0def3e5b45e970a8e8c0c
Michael <michael@discoursehosting.com>
2019-11-28 07:13:01 +0000

    working 'since'

--- plugin.rb
+++ plugin.rb
@@ -7,6 +7,36 @@
 
 enabled_site_setting :webhook_digest_enabled
 
+
+require 'enum_site_setting'
+
+class ::DigestIntervalSiteSetting < ::EnumSiteSetting
+  def self.valid_value?(val)
+    values.any? { |v| v[:value] == val.to_s }
+  end
+
+  def self.values
+    @values ||= [
+      { name: 'js.webhook_digest.interval.hour', value: '1' },
+      { name: 'js.webhook_digest.interval.halfday', value: '12' },
+      { name: 'js.webhook_digest.interval.day', value: '24' },
+      { name: 'js.webhook_digest.interval.twodays', value: '48' },
+      { name: 'js.webhook_digest.interval.week', value: '168' },
+      { name: 'js.webhook_digest.interval.twoweeks', value: '336' },
+      { name: 'js.webhook_digest.interval.month', value: '720' },
+      { name: 'js.webhook_digest.interval.twomonths', value: '1440' },
+      { name: 'js.webhook_digest.interval.sixmonths', value: '3268' },
+      { name: 'js.webhook_digest.interval.year', value: '8760' },
+    ]
+  end
+
+  def self.translate_names?
+    true
+  end
+end
+
+
+
 after_initialize {
 
   class ::Jobs::EnqueueDigestWebhooks  < Jobs::Scheduled
@@ -14,28 +44,60 @@ after_initialize {
     
     def execute(args)
       return unless SiteSetting.webhook_digest_enabled
-      DigestWebhooks.generate
+      hours = SiteSetting.webhook_digest_interval
+      DigestWebhooks.generate(hours, SiteSetting.webhook_digest_types.split(','))
     end
   end
   
   class ::DigestWebhooks
-    def self.generate
-      user_ids = Jobs::EnqueueDigestEmails.new.target_user_ids
-      user_ids = User.real.pluck(:id) # @debug 
+  
+    # return all users that haven't got a digest in the last X hours, and were not seen in the last X hours either.
+    def self.target_user_ids(hours)
+      query = User.real
+        .not_suspended
+        .activated
+        .where(staged: false)
+        .joins("LEFT JOIN user_custom_fields ON users.id = user_id AND user_custom_fields.name = 'last_digest_at'")
+        .where("GREATEST(TO_TIMESTAMP(value, 'YYYY-MM-DD HH24:MI:SS'), last_seen_at) < CURRENT_TIMESTAMP - ('1 HOUR'::INTERVAL * #{hours})")
+      query = query.where("approved OR moderator OR admin") if SiteSetting.must_approve_users?
+      query.pluck(:id)
+    end
+    
+    def self.generate(hours, types)
+      user_ids = target_user_ids(hours)
+      #user_ids = User.real.pluck(:id) # @debug 
       users = User.where(id: user_ids)
       return if users.blank?
       users.each do |user|
-        payload = {}
-        payload['json'] = UserJsonNotifications::json_digest(user)
-        digest = ::UserNotifications::digest(user)
-        payload['html'] = digest.html_part.body.raw_source
-        puts JSON.pretty_generate(payload) # @debug
-        # TODO send it
+        json = generate_for_user(hours, types, user)
+        send_to_webhook(json)
       end
     end
+    
+    def self.generate_for_user(hours, types, user)
+      payload = {}
+      opts = {}
+      opts[:since] = hours.hours.ago
+      
+      if types.include?('json')
+        payload['json'] = UserJsonNotifications::json_digest(user, opts)
+      end
+      
+      if types.include?('html')
+        digest = ::UserNotifications::digest(user, opts)
+        payload['html'] = digest.html_part.body.raw_source
+      end
+      
+      payload['user_id'] = user.id
+      payload['username'] = user.username
+      
+      puts JSON.pretty_generate(payload) # @debug
+      payload
+    end
+    
   end
   
-  #module ::PrependUserNotifications 
+
   class ::UserJsonNotifications 
 
     # need to copy some private methods from UserNotifications
@@ -91,10 +153,7 @@ after_initialize {
     # end copied methods
     
     def self.json_digest(user, opts = {})
-        opts = {}
-
         min_date = opts[:since] || user.last_emailed_at || user.last_seen_at || 1.month.ago
-        min_date = 1.month.ago # @debug
         digest_opts = { limit: SiteSetting.digest_topics + SiteSetting.digest_other_topics, top_order: true }
         topics_for_digest = Topic.for_digest(user, min_date, digest_opts).to_a
         if topics_for_digest.empty? && !user.user_option.try(:include_tl0_in_digests)
@@ -158,7 +217,7 @@ after_initialize {
           last_seen_at = short_date(user.last_seen_at || user.created_at)
           preheader_text = I18n.t('user_notifications.digest.preheader', last_seen_at: last_seen_at)
         end
-        result = [ :user_id => user.id, :username => user.username, :last_seen_at => last_seen_at, :preheader_text => preheader_text, :popular_topics => popular_topics.as_json, :counts => counts, :other_new_for_you => other_new_for_you.as_json ]
+        result = [ :last_seen_at => last_seen_at, :preheader_text => preheader_text, :popular_topics => popular_topics.as_json, :counts => counts, :other_new_for_you => other_new_for_you.as_json ]
         result
     end
   end
